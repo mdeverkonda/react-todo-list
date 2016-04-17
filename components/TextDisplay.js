ce5f35fa8b0c9e7f599ac9c0389b8e0634b95641
Madhav Deverkonda <mdeverk@XXXWMXXX-3.local>
2016-04-17 16:41:07 -0700

    Finished part 6

--- components/TextDisplay.js
+++ components/TextDisplay.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react'
 
 class TextDisplay extends Component {
 
+  handleClick(){
+      //console.log(this)
+      this.props.deleteLetter()
+  }
+
+
   render() {
-    return <div>I'm displaying text from my parent: {this.props.text}</div>
+    return <div>
+        <div>
+        <br/><br/>I'm displaying text from my parent: {this.props.text}
+            </div>
+        <br/><br/>
+        <button onClick={this.handleClick.bind(this)}>Delete 1 letter</button>
+      </div>
   }
 
 }
